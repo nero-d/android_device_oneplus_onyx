56c5d69fc2471a9ecd08895d33853f6c735cb1ad
nero-d <nihar.deshpande1810@gmail.com>
2017-01-09 05:29:59 +0000

    Modified device trees to build xosp.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,8 +48,9 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := cm_onyx_defconfig
+TARGET_KERNEL_CONFIG := cardinal_onyx_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/onyx
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,1 @@
-add_lunch_combo cm_onyx-user
-add_lunch_combo cm_onyx-userdebug
-add_lunch_combo cm_onyx-eng
+add_lunch_combo xosp_onyx-userdebug
